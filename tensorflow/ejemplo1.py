be286d99781edbad6f5170ecb03c8e7712ae1b8e
Olmo Zavala <olmozavala@gmail.com>
2018-05-31 11:33:42 -0400

    Improvig ejemplo1.py

--- tensorflow/ejemplo1.py
+++ tensorflow/ejemplo1.py
@@ -1,14 +1,18 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 trainX=np.linspace(-1,1,101)
 trainY=3*trainX + np.random.randn(*trainX.shape)*0.33
+
+plt.scatter(trainX, trainY)
+plt.show()
+
 X=tf.placeholder("float")
 Y=tf.placeholder("float")
 w = tf.Variable(0.0, name="weights")
 
 init= tf.global_variables_initializer()
-
 
 y_model=tf.mul(X,w)
 
